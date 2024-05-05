42820044c3c1b35cc189cbbbf9d42c95b8c861cc
Drew Howell <drewhowell1010@gmail.com>
2024-05-05 10:25:45 -0600

    triumphant message

--- Home.py
+++ Home.py
@@ -11,3 +11,6 @@ df = conn.query("SELECT * from fin.account;", ttl=600)
 # Print results.
 for row in df.itertuples():
     st.write(f"{row.NAME} has a :{row.ID}:")
+
+
+st.subheader('holy shit we are connected to a cloud snowflake database that is awesome')
